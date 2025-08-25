5a2c72011070862c446692d14613d6a1f3fef1c4
lijanahmmed <lijanahmmed@gmail.com>
2025-08-26 01:22:35 +0600

    added cashout section

--- js/home.js
+++ js/home.js
@@ -31,3 +31,33 @@ document.getElementById("add-money-btn")
     document.getElementById("add-pin").value = "";
   });
 
+//cashout money feature
+
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = parseInt(document.getElementById("withdraw-amount").value);
+
+  const availableBalance = parseInt(
+    document.getElementById("available-balance").innerText
+  );
+
+  const totalNewAvailableBalance = availableBalance - amount;
+
+  console.log(totalNewAvailableBalance);
+
+  document.getElementById("available-balance").innerText =
+    totalNewAvailableBalance;
+});
+
+// toggling feature
+
+document.getElementById("add-button").addEventListener("click", function (e) {
+  document.getElementById("cash-out-parent").style.display = "none";
+  document.getElementById("add-money-parent").style.display = "block";
+});
+document.getElementById("cash-out-button")
+  .addEventListener("click", function () {
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "block";
+  });
